49e2baae781b604d967b628748047a176edce9f4
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-05-01 12:11:58 -0700

    service worker async

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,11 @@ const FILES_TO_CACHE = [
   '/icons/icon-72x72.png',
   '/icons/icon-96x96.png',
   '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png'
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 // Install the service worker
@@ -24,7 +28,7 @@ self.addEventListener('install', function(evt) {
     })
   );
 
-  self.skipWaiting();
+  //self.skipWaiting();
 });
 
 // Activate the service worker and remove old data from the cache
@@ -51,20 +55,17 @@ self.addEventListener('fetch', function(evt) {
     evt.respondWith(
       caches
         .open(DATA_CACHE_NAME)
-        .then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
+        .then(async cache => {
+          try {
+            const response = await fetch(evt.request);
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          } catch (err) {
+            return await cache.match(evt.request);
+          }
         })
         .catch(err => console.log(err))
     );
@@ -73,15 +74,14 @@ self.addEventListener('fetch', function(evt) {
   }
 
   evt.respondWith(
-    fetch(evt.request).catch(function() {
-      return caches.match(evt.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (evt.request.headers.get('accept').includes('text/html')) {
-          // return the cached home page for all requests for html pages
-          return caches.match('/');
-        }
-      });
+    fetch(evt.request).catch(async function() {
+      const response = await caches.match(evt.request);
+      if (response) {
+        return response;
+      } else if (evt.request.headers.get('accept').includes('text/html')) {
+        // return the cached home page for all requests for html pages
+        return caches.match('/');
+      }
     })
   );
 });
