0b53d0d18ba960dc40ed3c1c5493e6a25c7a8073
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-05-01 14:51:42 -0700

    cache issue

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,20 +4,20 @@ const CACHE_NAME = 'budget-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v2';
 
 const FILES_TO_CACHE = [
-  '/',
-  './index.html',
-  './manifest.json',
-  './js/index.js',
-  './js/idb.js',
-  '/css/style.css',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
+   '/',
+   './index.html',
+   './manifest.json',
+   './js/index.js',
+   './js/idb.js',
+   '/css/style.css',
+   '/icons/icon-72x72.png',
+   '/icons/icon-96x96.png',
+   '/icons/icon-128x128.png',
+   '/icons/icon-144x144.png',
+   '/icons/icon-152x152.png',
+   '/icons/icon-192x192.png',
+   '/icons/icon-384x384.png',
+   '/icons/icon-512x512.png'
 ];
 
 // Install the service worker
@@ -32,6 +32,23 @@ self.addEventListener('install', function(evt) {
   self.skipWaiting();
 });
 
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
 
 
 // Intercept fetch requests
@@ -71,20 +88,3 @@ self.addEventListener('fetch', function(evt) {
   );
 });
 
-// Activate the service worker and remove old data from the cache
-self.addEventListener('activate', function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
